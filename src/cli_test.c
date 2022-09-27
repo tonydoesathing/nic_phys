0668f5720a37db5a8cd1ae410d7f5137e2632f72
Tony <tonygovernmentemail@gmail.com>
2022-09-27 14:07:31 -0600

    Finished the CLI

--- src/cli_test.c
+++ src/cli_test.c
@@ -7,9 +7,57 @@
 */
 int main(void){
     bool running = true;
+    int err;
+    char command;
+    unsigned char bitpattern;
+
     while(running){
         // take in input
-        
+        // get command
+        printf("Enter r to receive, s to send, q to quit:");
+        err = scanf(" %c", &command);
+        printf("\n");
+        // check for errors
+        if(err!=1){
+            printf("Improper command \n");
+        }else{
+            // process the commands
+            // quit the application
+            if(command=='q'){
+                running=false;
+                break;
+            }
+            
+            if(command=='r'){
+                // recv 
+                bitpattern = nic_recv();
+                printf("Received: %d \n", bitpattern);
+                for (unsigned char mask = 0x08; mask != 0; mask>>=1) {
+                    printf("%d",(bitpattern & mask ) ? 1:0);
+                }
+                printf("\n");
+
+            }else if(command=='s'){
+                // send
+                
+                // get bitpattern
+                printf("Enter the bitpattern to send (e.g. 1001): \n");
+                int holder;
+                bitpattern=0;
+                for(int i = 0; i<4; i++){
+                    printf("Port # %d (0 or 1): ", 4-i);
+                    err = scanf(" %d", &holder);
+                    if(err!=1){
+                        printf("Improper input\n");
+                        break;
+                    }
+                    printf("\n");
+                    bitpattern += holder << i;
+                }
+                nic_send(bitpattern);
+            }
+        }
     }
+
 }
 
